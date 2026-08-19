1662f4efe5f8b70f4dd16762ca01eafa56f9c857
Thibaud Collyn <thibaudcollyn@gmail.com>
2026-08-19 17:23:57 +0200

    Exploring basic api calls and routes folder.

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
-const express = require('express');
+import express from "express";
+import campgroundRoutes from "./routes/campgroundRoutes.js";
+
 const app = express();
+
+app.use("/campgrounds", campgroundRoutes);
+
 const port = 5001;
 
 const server = app.listen(port, () => {
